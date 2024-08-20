e3241f3265bfeb11a3ba6b46202cbf0b744ee82b
Tom Roussel <tom.roussel@segments.ai>
2024-08-20 13:34:42 +0200

    Removed unused code

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,12 +1,8 @@
 from dataclasses import dataclass
-import codecs
-import json
-import base64
 from pathlib import Path
 
 import numpy as np
 import fiftyone as fo
-import fiftyone.operators.types as types
 import segments
 
 
@@ -18,27 +14,6 @@ class Point3D:
 
     def array(self):
         return np.array((self.x, self.y, self.z))
-
-
-def in_cache(ctx, cache_key: str) -> bool:
-    return cache_key in ctx.params
-
-
-def fetch_cache(ctx, cache_key: str) -> dict:
-    cache_data = ctx.params[cache_key]
-    decoded = base64.decodebytes(cache_data.encode("utf-8"))
-    decoded = codecs.decode(decoded, encoding="zlib_codec").decode("utf-8")
-    decoded = json.loads(decoded)
-
-    return decoded
-
-
-def add_cache(inputs, cache_key: str, cache_data: list[dict]):
-    encoded = json.dumps(cache_data)
-    encoded = codecs.encode(encoded.encode("utf-8"), encoding="zlib_codec")
-    encoded = base64.encodebytes(encoded).decode("utf-8")
-
-    inputs.str(cache_key, default=encoded, view=types.HiddenView())
 
 
 def pcd_filename_map(dataset: fo.Dataset) -> dict[str, fo.Sample]:
